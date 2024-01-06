4249472caa341c3f06ae938ff37090a20f08150e
unknown <21PW17@cs2k16.amcs.ws2k12.psgcs.edu>
2024-01-06 17:03:05 +0530

    level 1b

---
+++ level_1b.py
@@ -0,0 +1,87 @@
+import json
+
+f_read = open('level1b.json')
+
+data = json.load(f_read)
+
+quantities = []
+rest_distances = []
+neighbourhoods = []
+for i in data["neighbourhoods"]:
+	neighbourhoods.append(i)
+        
+for j in data["restaurants"]["r0"]["neighbourhood_distance"]:
+    rest_distances.append(j)
+rest_distances.insert(0, 0)
+
+graph = []
+index = 1
+for i in neighbourhoods:
+    data["neighbourhoods"][i]["distances"].insert(0, rest_distances[index])
+    index += 1
+    graph.append(data["neighbourhoods"][i]["distances"])
+    quantities.append(data["neighbourhoods"][i]["order_quantity"])
+    
+graph.insert(0, rest_distances)
+#print(graph)
+
+max = 1120
+
+
+def nearest_neighbor(graph):
+    num_neigh = len(graph)
+    unvisited_neigh = [i for i in range(1, num_neigh)]
+    current_neigh = 0
+    tours = []
+    tour = [current_neigh]
+
+    
+    visited_path, unvisited_check = visited(unvisited_neigh, current_neigh, tour)
+    tours.append(visited_path)
+
+    while unvisited_check:
+        current_neigh = 0
+        tour = [current_neigh]
+        visited_path, unvisited_check = visited(unvisited_check, current_neigh, tour)
+        tours.append(visited_path)
+
+    return tours
+
+def visited(unvisited_neigh, current_neigh, tour):
+    cost = 0
+    while unvisited_neigh:
+        nearest_neigh = min(unvisited_neigh, key=lambda neigh: graph[current_neigh][neigh])
+        ind = unvisited_neigh.index(nearest_neigh)
+        cost += quantities[ind]
+        if cost > max:
+            cost -= quantities[ind]
+            tour.append(tour[0])
+            return tour, unvisited_neigh
+        tour.append(nearest_neigh)
+        quantities.pop(ind)
+        unvisited_neigh.remove(nearest_neigh)
+        current_neigh = nearest_neigh
+    tour.append(tour[0])
+    return tour, unvisited_neigh
+result_tour = nearest_neighbor(graph)
+
+for i in result_tour:
+    i[0] = i[len(i) - 1] = "r0"
+    for j in range(1, len(i)-1):
+        i[j] = 'n' + str(i[j]-1)
+
+result = {}
+paths = []
+for k in range(len(result_tour)):
+    paths.append("path" + str(k+1))
+
+for k in range(len(result_tour)):
+    result[paths[k]] = result_tour[k]
+
+result = dict(v0 = (result))
+
+
+with open('level1b_output.json', 'w') as f_write:
+    json.dump(result, f_write)
+
+f_read.close()
---
+++ level_2a.py
@@ -0,0 +1,114 @@
+import json
+
+f_read = open('level2a.json')
+
+data = json.load(f_read)
+
+quantities = []
+rest_distances = []
+neighbourhoods = []
+for i in data["neighbourhoods"]:
+	neighbourhoods.append(i)
+        
+for j in data["restaurants"]["r0"]["neighbourhood_distance"]:
+    rest_distances.append(j)
+rest_distances.insert(0, 0)
+
+graph = []
+index = 1
+for i in neighbourhoods:
+    data["neighbourhoods"][i]["distances"].insert(0, rest_distances[index])
+    index += 1
+    graph.append(data["neighbourhoods"][i]["distances"])
+    quantities.append(data["neighbourhoods"][i]["order_quantity"])
+    
+graph.insert(0, rest_distances)
+#print(graph)
+
+vehicle_list = []
+capacity_list = []
+
+for i in data["vehicles"]:
+    vehicle_list.append(i)
+
+for j in vehicle_list:
+    capacity_list.append(data["vehicles"][j]["capacity"])
+
+print(vehicle_list)
+print(capacity_list)
+
+
+def nearest_neighbor(graph):
+    num_neigh = len(graph)
+    unvisited_neigh = [i for i in range(1, num_neigh)]
+    current_neigh = 0
+    tours = []
+    tour = [current_neigh]
+    capacity = capacity_list[0]
+
+    
+    visited_path, unvisited_check = visited(unvisited_neigh, current_neigh, tour, capacity)
+    tours.append(visited_path)
+
+    while unvisited_check:
+        capacity_list.append(capacity_list[0])
+        capacity_list.pop(0)
+        capacity = capacity_list[0]
+        current_neigh = 0
+        tour = [current_neigh]
+        visited_path, unvisited_check = visited(unvisited_check, current_neigh, tour, capacity)
+        tours.append(visited_path)
+
+    return tours
+
+def visited(unvisited_neigh, current_neigh, tour, capacity):
+    cost = 0
+    while unvisited_neigh:
+        nearest_neigh = min(unvisited_neigh, key=lambda neigh: graph[current_neigh][neigh])
+        ind = unvisited_neigh.index(nearest_neigh)
+        cost += quantities[ind]
+        if cost > capacity:
+            cost -= quantities[ind]
+            tour.append(tour[0])
+            return tour, unvisited_neigh
+        tour.append(nearest_neigh)
+        quantities.pop(ind)
+        unvisited_neigh.remove(nearest_neigh)
+        current_neigh = nearest_neigh
+    tour.append(tour[0])
+    return tour, unvisited_neigh
+result_tour = nearest_neighbor(graph)
+
+for i in result_tour:
+    i[0] = i[len(i) - 1] = "r0"
+    for j in range(1, len(i)-1):
+        i[j] = 'n' + str(i[j]-1)
+
+result = {}
+paths = []
+for k in range(len(result_tour)//len(vehicle_list)):
+    paths.append("path" + str(k+1))
+
+new_result = []
+for x in range(len(vehicle_list)):
+   new_result.append([])
+print(result_tour[0])
+print(new_result[0])
+count = 0
+while result_tour:
+    for i in range(len(vehicle_list)):
+        new_result[i].append(result_tour[count])
+        result_tour.pop(0)
+print(new_result)
+'''
+for k in range(len(result_tour)):
+    result[paths[k]] = result_tour[k]
+
+result = dict(v0 = (result))
+print(result)
+
+with open('level1b_output.json', 'w') as f_write:
+    json.dump(result, f_write)
+
+f_read.close()
+'''
